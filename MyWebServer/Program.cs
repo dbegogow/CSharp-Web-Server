77e2c1149494c356861da07c018004b1fbb9b289
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-06-03 13:28:52 +0300

    Choose the localhost IP address and a free local port

---
+++ MyWebServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace MyWebServer
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var address = IPAddress.Parse("127.0.0.1");
+            var port = 8080;
+
+            var serverListener = new TcpListener(address, port);
+
+            serverListener.Start();
+
+            Console.WriteLine($"Server started on port {port}...");
+            Console.WriteLine("Listening for requests...");
+
+            var connection = await serverListener.AcceptTcpClientAsync();
+        }
+    }
+}
