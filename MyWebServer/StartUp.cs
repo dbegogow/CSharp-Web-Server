6be5fa674d051273680154ce1f9c3ae832214cbc
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-06-04 15:04:09 +0300

    Extract separate Server and HTTP classes

---
+++ MyWebServer/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using MyWebServer.Server;
+using System.Threading.Tasks;
+
+namespace MyWebServer
+{
+    class StartUp
+    {
+        public static async Task Main()
+        {
+            var server = new HttpServer("127.0.0.1", 9090);
+
+            await server.Start();
+        }
+    }
+}
