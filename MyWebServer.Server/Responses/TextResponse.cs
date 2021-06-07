c649f54fb8f3996acc4d4d24c54d25f0864306f8
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-06-07 13:07:43 +0300

    Create content response and html response

--- MyWebServer.Server/Responses/TextResponse.cs
+++ MyWebServer.Server/Responses/TextResponse.cs
@@ -1,26 +1,11 @@
-﻿using System.Text;
-using MyWebServer.Server.Common;
-using MyWebServer.Server.Http;
+﻿using MyWebServer.Server.Http;
 
 namespace MyWebServer.Server.Responses
 {
-    public class TextResponse : HttpResponse
+    public class TextResponse : ContentResponse
     {
-        public TextResponse(string text, string contentType)
-            : base(HttpStatusCode.OK)
-        {
-            Guard.AgainstNull(text);
-
-            var contentLength = Encoding.UTF8.GetByteCount(text).ToString();
-
-            this.Headers.Add("Content-Type", contentType);
-            this.Headers.Add("Content-Length", contentLength);
-
-            this.Content = text;
-        }
-
         public TextResponse(string text)
-            : this(text, "text/plain; charset=UTF-8")
+            : base(text, "text/plain; charset=UTF-8")
         {
         }
     }
--- MyWebServer/StartUp.cs
+++ MyWebServer/StartUp.cs
@@ -8,7 +8,7 @@ namespace MyWebServer
     {
         public static async Task Main()
             => await new HttpServer(routes => routes
-                    .MapGet("/", new TextResponse("Hello from Ivo!"))
+                    .MapGet("/", new TextResponse("Hello from Dzhulio!"))
                     .MapGet("/Cats", new TextResponse("<h1>Hello from the cats!</h1>", "text/html"))
                     .MapGet("/Dogs", new TextResponse("<h1>Hello from the dogs!</h1>", "text/html")))
                 .Start();
